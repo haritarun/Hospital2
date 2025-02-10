e9748ac91f4d514fbfda3673f5ec98a058b7b445
haritarun <tarunbommana798@gmail.com>
2025-02-11 01:39:33 +0530

    ..

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],  
   server: {
-    proxy: {
-        '/api': 'http://localhost:3000',
-    },
-},
+    host: '0.0.0.0',  // Ensure the server is accessible externally
+    port: 5173,        // Change port if necessary
+  },
 })
